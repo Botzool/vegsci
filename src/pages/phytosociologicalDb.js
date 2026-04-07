355cb1a84fcd3a6ceb65f838d7deef61e454f546
Lenka H <hubackova.lenka@gmail.com>
2026-04-07 09:40:51 +0200

    redirect fix

---
+++ src/pages/phytosociologicalDb.js
@@ -0,0 +1,11 @@
+import { useEffect } from "react";
+
+const PhytosociologicalDbPage = () => {
+  useEffect(() => {
+    window.location.replace("https://czechveg.github.io/");
+  }, []);
+
+  return null;
+};
+
+export default PhytosociologicalDbPage;
